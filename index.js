167b8e5edd8126eeee1a4af5cdb5fc087bc3ea4d
Intelligent Dive <info@intelligentdive.com>
2023-04-14 15:36:34 +0600

    new add

---
+++ index.js
@@ -0,0 +1,9 @@
+import react from "React"
+import * as Layout from "./src/Layout"
+import * as Conponents from "./src/Components"
+
+import './src/styles/gobal.css'
+function isIdui(string){
+    return string === 'react-id-ui'
+}
+module.exports = isIdui;
